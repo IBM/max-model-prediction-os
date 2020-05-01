f850ab63c5d69c3262f2c2d152178a7e1903b7bb
ptitzler <ptitzler@us.ibm.com>
2020-05-01 12:34:26 -0700

    updates

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -9,8 +9,26 @@ fi
 
 APP_LABEL=$1 
 
+echo "Removing resources associated with ${APP_LABEL} ..."
+
 # delete all resources associated with the deployed app
 oc delete all -l app=${APP_LABEL}
 
-# delete the image stream
-oc delete imagestream ${APP_LABEL}
+# delete individual resources
+#oc delete route ${APP_LABEL}
+#oc delete deployment ${APP_LABEL}
+#oc delete service ${APP_LABEL}
+#oc delete imagestream ${APP_LABEL}
+
+echo "Listing resources associated with ${APP_LABEL} ..."
+
+oc get all | grep ${APP_LABEL}
+
+RETURN_CODE=$?
+if [[ $RETURN_CODE -eq 0 ]]
+then
+    echo "Error. One or more resources were not removed."
+    exit 1
+fi
+
+echo "All resources associated with ${APP_LABEL} were removed."
